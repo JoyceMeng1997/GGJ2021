459c71d8b5a4acadc4f620b840c3ecc33d778276
Joyce_Meng <JoyceMeng1997@users.noreply.github.com>
2021-02-06 13:28:00 +0800

    UI脚本和小功能

---
+++ Project/DoneLAF/Assets/Scripts/Scenes/Menu_Instruction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Menu_Instruction : MonoBehaviour
+{
+    private Transform inTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inTrans = this.gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void showpanel()
+    {
+        if(this.isActiveAndEnabled)
+        {
+            inTrans.DOLocalMoveY(0,0.5f, false);
+        }
+    }
+    public void hidepanel()
+    {
+        if(this.isActiveAndEnabled)
+        {
+            inTrans.DOLocalMoveY(-540,0.5f, false);
+        }
+    }
+}
